096007bb60d6766ce5eca9a1f86b4f70c54ec3c2
SoinRoma <61582942+SoinRoma@users.noreply.github.com>
2020-11-01 00:23:26 +0500

    Add files via upload

---
+++ Panteleev(Complier)/Calculator/grammar.h
@@ -0,0 +1,34 @@
+
+enum Token_value : char {
+
+    NUMBER,
+    END,
+
+    PLUS = '+',
+    MINUS = '-',
+    MULTIPLICATION = '*',
+    DIVISION = '/',
+
+    SEMICOLON = ';',
+    EQUALITY = '=',
+
+    LP = '(',
+    RP = ')',
+
+
+    CONST
+};
+
+enum Number_value : char {
+    NUMBER_0 = '0',
+    NUMBER_1 = '1',
+    NUMBER_2 = '2',
+    NUMBER_3 = '3',
+    NUMBER_4 = '4',
+    NUMBER_5 = '5',
+    NUMBER_6 = '6',
+    NUMBER_7 = '7',
+    NUMBER_8 = '8',
+    NUMBER_9 = '9',
+    NUMBER_P = '.',
+};
